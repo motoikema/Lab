e9628df94cc6b10394c27b852c1c43a0570d5bb9
Moto Ikema <motoikema@gmail.com>
2017-02-11 18:04:15 +0900

    add LearnRuby/

---
+++ LearnRuby/ex9.rb
@@ -0,0 +1,20 @@
+# Here's some new strang stuff, remember type it exactly.
+
+days = "Mon The Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+test = "aaa
+bbb"
+
+puts test
+puts "test"
+puts #{test}
+puts "#{test}"
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"
